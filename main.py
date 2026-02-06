972a88ecc2ca910b97d93b8330c43b3ddc59531d
Nilesh Thakur <142657051+Lethality-God@users.noreply.github.com>
2026-02-06 20:55:40 +0530

    Update main.py
    Added threshold tuning and fixed minor data leakage where training data was vectorized before train-test split.

--- main.py
+++ main.py
@@ -1,41 +1,34 @@
 import pandas as pd
+import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split as TTS
 from sklearn.linear_model import LogisticRegression as LR
+from sklearn.metrics import classification_report, confusion_matrix
 
 df = pd.read_csv("mail_data.csv")
 
 df.Message = df.Message.str.lower()
 vectorizer = TfidfVectorizer(stop_words='english', ngram_range=(1,2), min_df=2)
 
-X = vectorizer.fit_transform(df.Message)
 y = df.Category
+X_train, X_test, y_train, y_test = TTS(df.Message, y, random_state=0)
 
-X_train, X_test, y_train, y_test = TTS(X,y,random_state=0)
-model = LR(class_weight="balanced", C = 100, max_iter=1000)
-model.fit(X_train, y_train)
+X_train = vectorizer.fit_transform(X_train)
+X_test = vectorizer.transform(X_test)
 
-predicted = model.predict(X_test)
 
-tp,fp,tn,fn = 0,0,0,0
+def model_generator(X_train, X_test, y_train, y_test, threshold):
 
-for i in range(0, len(y_test)):
-    pre = predicted[i]
-    act = y_test.iloc[i]
+    model = LR(class_weight="balanced", C = 100, max_iter=1000)
+    model.fit(X_train, y_train)
 
-    if(pre=='spam' and act == 'spam'):
-        tp += 1
+    spam_prob = model.predict_proba(X_test)[:, 1]
+    predicted = np.where(spam_prob >= threshold, 'spam', 'ham')
 
-    elif(pre=='spam' and act=='ham'):
-        fp += 1
+    print(f"Report for thereshold {threshold}:")
+    print(classification_report(y_test, predicted))
 
-    elif(pre=='ham' and act == 'spam'):
-        fn += 1
+    print(confusion_matrix(y_test, predicted))
 
-    else:
-        tn+=1
+model_generator(X_train, X_test, y_train, y_test, 0.6)
 
-print(f"True Positive = {tp} \n True Negative = {tn} \n False Positive = {fp} \n False Negative = {fn}")
-print(f"Accuracy: {(tp+tn)/(tp+tn+fp+fn)*100}")
-print(f"Recall: {tp/(tp+fn)*100}")
-print(f"Precision: {tp/(tp+fp)*100}")
